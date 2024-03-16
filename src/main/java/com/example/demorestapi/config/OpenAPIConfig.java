b9013699bcf31f281977f212fb6c2a9f7c90949a
Entint <toancule2001@gmail.com>
2024-03-17 01:15:10 +0700

    Config CORS

--- src/main/java/com/example/demorestapi/config/OpenAPIConfig.java
+++ src/main/java/com/example/demorestapi/config/OpenAPIConfig.java
@@ -1,7 +1,10 @@
 package com.example.demorestapi.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +12,10 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAPIConfig {
     @Bean
     public OpenAPI springOpenAPI() {
-        return new OpenAPI().info((new Info()
+        return new OpenAPI().addServersItem(new Server().url("https://demospringboot-production-7e99.up.railway.app"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info((new Info()
                 .title("SpringDoc API Test")
                 .description("SpringDoc Simple Application Test")
                 .version("1.0")
