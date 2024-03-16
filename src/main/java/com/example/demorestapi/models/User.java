abd87c9f51f43a1c48edd56aa20e3ca4887b746b
Entint <toancule2001@gmail.com>
2024-03-16 23:09:48 +0700

    Config CORS

--- src/main/java/com/example/demorestapi/models/User.java
+++ src/main/java/com/example/demorestapi/models/User.java
@@ -4,29 +4,18 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private int id;
-    @Getter
-    @Setter
     private String userName;
-    @Getter
-    @Setter
     private String password;
-    @Getter
-    @Setter
     private String email;
-    @Getter
-    @Setter
     private String numberPhone;
-    @Getter
-    @Setter
-    private String AvtImage;
     @Override
     public String toString() {
         return "{" +
@@ -34,7 +23,6 @@ public class User {
                 ", userName='" + userName + '\'' +
                 ", email='" + email + '\'' +
                 ", numberPhone='" + numberPhone + '\'' +
-                ", AvtImage='" + AvtImage + '\'' +
                 '}';
     }
 }
